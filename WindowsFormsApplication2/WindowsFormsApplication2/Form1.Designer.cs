6c07354b8e678ba36cfd54ad4899d0df08cf9d86
GuusHamm <guushamm@gmail.com>
2015-03-26 09:53:12 +0100

    Toevoeging opdracht week 6

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBubble = new System.Windows.Forms.Button();
+            this.btnInsertion = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnBubble
+            // 
+            this.btnBubble.Location = new System.Drawing.Point(61, 182);
+            this.btnBubble.Name = "btnBubble";
+            this.btnBubble.Size = new System.Drawing.Size(75, 23);
+            this.btnBubble.TabIndex = 0;
+            this.btnBubble.Text = "Bubble Sort";
+            this.btnBubble.UseVisualStyleBackColor = true;
+            this.btnBubble.Click += new System.EventHandler(this.btnBubble_Click);
+            // 
+            // btnInsertion
+            // 
+            this.btnInsertion.Location = new System.Drawing.Point(210, 182);
+            this.btnInsertion.Name = "btnInsertion";
+            this.btnInsertion.Size = new System.Drawing.Size(142, 23);
+            this.btnInsertion.TabIndex = 1;
+            this.btnInsertion.Text = "Insertion Sort";
+            this.btnInsertion.UseVisualStyleBackColor = true;
+            this.btnInsertion.Click += new System.EventHandler(this.btnInsertion_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnInsertion);
+            this.Controls.Add(this.btnBubble);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBubble;
+        private System.Windows.Forms.Button btnInsertion;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -12,9 +12,74 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        private int[] array = { 40, 2, 1, 43, 433, 65, 0, 11, 58, 1223, 42, 4,123,1444,123123,435,35,6,75,3,132,23,5,67,89,65,4,25,5,6546,45,62,46,723, };
+
         public Form1()
         {
             InitializeComponent();
+            ListUpdater();
+           }
+
+        private void btnBubble_Click(object sender, EventArgs e)
+        {
+            int tempvalue = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        tempvalue = array[i];
+                        array[i] = array[j];
+                        array[j] = tempvalue;
+
+                    }
+                }
+            }
+
+            ListUpdater();
         }
+
+        private void ListUpdater()
+        {
+            listBox1.Items.Clear();
+
+            foreach (int i in array)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void btnInsertion_Click(object sender, EventArgs e)
+        {
+            int tempvalue = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                int j = i;
+
+                while (j > 0)
+                {
+                    if (array[j-1] > array [j])
+                    {
+                        tempvalue = array[j-1];
+                        array[j - 1] = array[j];
+                        array[j] = tempvalue;
+                        j--;
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                ListUpdater();
+                
+            }
+
+        }
+
     }
 }
