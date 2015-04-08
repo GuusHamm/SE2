716327f5d7caf1af0e07a4e0a37bfe8c99a230a8
GuusHamm <guushamm@gmail.com>
2015-04-08 15:59:54 +0200

    Created Functioning File Serializer

--- W7/W7/Form1.cs
+++ W7/W7/Form1.cs
@@ -5,42 +5,82 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace W7
 {
     public partial class Form1 : Form
     {
+        private List<Player> playerList;
         public Form1()
         {
             InitializeComponent();
+            playerList = new List<Player>();
             CsVConverter();
+           
 
         }
 
         public void CsVConverter()
         {
-
-            FileStream file;        // koppeling met bestand
-            StreamReader reader;    // interpreteer als tekst
-            try
+            
+            using (StreamReader r = new StreamReader(@"C:\Users\Guus\Source\Repos\SE2\W7\W7\cust.csv"))
+                
             {
-                file = new FileStream("file.txt", FileMode.Open,
-                                                  FileAccess.Read);
-                reader = new StreamReader(file);
-                string line = reader.ReadLine();
+                string file_contents = " ";
+                while (!r.EndOfStream)
+                {
+                    file_contents = r.ReadLine();
+                    int indexofcomma = file_contents.IndexOf(',', 0);
+                    
 
-                reader.Close();
-                file.Close();
+                    string playername = file_contents.Substring(0, indexofcomma);
+                    int oldindex = indexofcomma;
+
+                    indexofcomma = file_contents.IndexOf(',', indexofcomma + 1);
+
+                    int dollar = Convert.ToInt32(file_contents.Substring(oldindex + 1, indexofcomma - oldindex - 4));
+                    oldindex  = indexofcomma;
+
+                    indexofcomma = file_contents.Length;
+                    int euro = Convert.ToInt32(file_contents.Substring(oldindex + 1, indexofcomma - oldindex - 4));
+
+                    Player player = new Player(playername, dollar, euro);
+                    
+                    playerList.Add(player);
+
+                }
+                BinaryFormatter fmt;    // interpreteer als binair object
+
+                using (FileStream g = new FileStream("file.bin", FileMode.Create, FileAccess.ReadWrite))
+                {
+
+                }
+                foreach (Player player in playerList)
+                {
+                    using (FileStream f = new FileStream("file.bin", FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        
+                        fmt = new BinaryFormatter();
+                        fmt.Serialize(f, player);             // schrijf naar file 
+                    }
+                }
+               
+
+                
             }
-            catch (IOException) { }
 
-            reader.Read()
+            
+
 
         }
+
     }
 
 }
---
+++ W7/W7/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W7
+{
+    [Serializable]
+    class Player
+    {
+        public string PlayerName;
+        public double DollarBankRoll;
+        public double EuroBankRoll;
+
+        public Player(string playerName, double dollarBankRoll, double euroBankRoll)
+        {
+            this.PlayerName = playerName;
+            this.DollarBankRoll = dollarBankRoll;
+            this.EuroBankRoll = euroBankRoll;
+        }
+    }
+}
